09bc7a9ecb382e7e4c3d001704cc9cc70fbbf5a9
ArthurAizawa <139519439+ArthurAizawa@users.noreply.github.com>
2024-09-02 14:53:21 -0300

    Implements DepartmentDaoJDBC

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,15 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
     //expondo o metodo da interface mas instanciando a suplementação
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
+    }
+    public static DepartmentDao createDepartmentDao(){
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
